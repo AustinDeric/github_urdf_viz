f942ce3e926f0fa7695b5035ad2122f765892c00
AustinDeric <Austin.Deric@gmail.com>
2017-05-07 21:29:43 -0500

    add list_robots method

--- application.py
+++ application.py
@@ -13,6 +13,30 @@ application = Flask(__name__)
 def root_index():
     return render_template('index.html')
 
+@application.route('/user')
+def list_robots():
+    owner = 'AustinDeric'
+    repo = 'abb_experimental'
+    branch = 'irb1600-dae'
+    sha_url = 'https://api.github.com/repos/{}/{}/branches/{}'.format(owner, repo, branch)
+    r = requests.get(sha_url)
+    sha_data = json.loads(r.text)
+    sha = sha_data['commit']['sha']
+
+    tree_payload = {'recursive': '1'}
+    tree_url = 'https://api.github.com/repos/{}/{}/git/trees/{}'.format(owner, repo, sha)
+    x = requests.get(tree_url, tree_payload)
+    tree_data = json.loads(x.text)
+    robots = []
+    for i in tree_data['tree']:
+        if PurePath(i['path']).suffix == '.launch':
+            if ((PurePath(i['path']).stem)[0:5] == 'load_'):
+                robot_name = PurePath(i['path']).stem[5:]
+                new_path = 'localhost:5000/{}/{}/{}/{}'.format(owner, repo, branch, robot_name)
+                robots.append({'href': new_path, 'caption': robot_name})
+
+    return render_template('list_robots.html', robots=robots)
+
 
 # run the app.
 if __name__ == "__main__":
